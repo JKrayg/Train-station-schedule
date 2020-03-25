4701cbef5affcbd1fbd19e361c410a5b56b7519a
Jake Krayger <jkrayg@protonmail.com>
2020-03-24 21:08:09 -0400

    Adding firebase and database function to add values to table

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,72 @@
-var newTR = $("<tr>");
-var newTD = $("<td>");
+var firebaseConfig = {
+    apiKey: "AIzaSyD_-IWOWvOWHSuCkko7G0crYLy8YT2xQ8M",
+    authDomain: "trainschedule-478a8.firebaseapp.com",
+    databaseURL: "https://trainschedule-478a8.firebaseio.com",
+    projectId: "trainschedule-478a8",
+    storageBucket: "trainschedule-478a8.appspot.com",
+    messagingSenderId: "131970228692",
+    appId: "1:131970228692:web:21e7b5a3bff972552f7641"
+};
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
 
 
 
 
-
-$("button").on("click", function(e) {
+$("button").on("click", function (e) {
     e.preventDefault();
-    
+    var tyme = new Date();
+
+    var currentHour = tyme.getHours();
+    var currentMinute = ("0" + tyme.getMinutes(currentMinute)).slice(-2);
+    var currentTime = currentHour + ":" + currentMinute;
+    //var nextTrain = currentTime ;
+
+    //currentMinute = currentMinute % 15;
+    //console.log(currentMinute);
+
     var trainName = $("#train-name").val().trim();
-    newTD.text(trainName);
-    newTD.appendTo(newTR);
-    newTR.appendTo($("table"));
     var destName = $("#destination-name").val().trim();
-    var trainTime = $("#train-time").val().trim();
     var timeFreq = $("#time-freq").val().trim();
     
-})
+    
 
+    database.ref().set({
+        trainName:trainName,
+        destName:destName,
+        timeFreq:timeFreq,
+        currentTime:currentTime
+    })
 
+    database.ref().on("value", function(info){
+        var newTR = $("<tr>").append(
+            $("<td>").text(info.val().trainName),
+            $("<td>").text(info.val().destName),
+            $("<td>").text(info.val().timeFreq),
+            $("<td>").text(info.val().currentTime)
+        );
+        newTR.appendTo("tbody");
+
+    })
+
+    localStorage.clear();
+
+      // Store all content into localStorage
+        localStorage.setItem("trainName", trainName);
+        localStorage.setItem("destName", destName);
+        localStorage.setItem("timeFreq", timeFreq);
+        localStorage.setItem("currentTime", currentTime);
+    })
+
+    newTR = $("<tr>").append(
+        $("<td>").text(localStorage.getItem("trainName")),
+        $("<td>").text(localStorage.getItem("destName")),
+        $("<td>").text(localStorage.getItem("timeFreq")),
+        $("<td>").text(localStorage.getItem("currentTime"))
+    );
+
+    $('#train-name').val('');
+    $('#destination-name').val('');
+    $('#time-freq').val('');
+    $('#train-time').val('');

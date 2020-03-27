3ee07e648eed37722d37881c4d7a91ba0a176214
Jake Krayger <jkrayg@protonmail.com>
2020-03-26 21:33:09 -0400

    Adding current time clock and fixing firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,9 @@
 var firebaseConfig = {
     //
     //
+    //
+    //
+    //
     //Add API key!
     apiKey: "",
     authDomain: "trainschedule-478a8.firebaseapp.com",
@@ -16,6 +19,15 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 
+function update() {
+    var currentTime = moment().format("hh:mm a");
+    $("#current-time").html("Current Time: " + currentTime);
+}
+
+setInterval(update, 1000);
+
+
+
 
 // on.click function for submit button
 $("button").on("click", function (e) {
@@ -27,71 +39,79 @@ $("button").on("click", function (e) {
     var trainTime = $("#train-time").val().trim();
     var timeFreq = $("#time-freq").val().trim();
 
+    var firstTrainTime = moment(trainTime, "hh:mm a").subtract(1, "years");
+    var timeDiff = moment().diff(moment(firstTrainTime), "minutes");
+    var timeLeft = timeDiff % timeFreq;
+    var minutesAway = timeFreq - timeLeft;
+    var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
+
     var newTrain = {
         name: trainName,
         destination: destName,
         firstTrain: trainTime,
-        frequency: timeFreq
+        frequency: timeFreq,
+        nextArrival: nextArrival,
+        minutesAway: minutesAway
     };
 
 
     // Send info to firebase
-    database.ref().set(newTrain);
+    database.ref().push(newTrain);
+
 
     console.log(newTrain.name);
     console.log(newTrain.destination);
     console.log(newTrain.frequency);
 
-    //return info to html
-    database.ref().on("value", function (info) {
-
-        var newTime = info.val().firstTrain;
-        var newFreq = info.val().frequency;
-
-
-        var firstTrainTime = moment(newTime, "hh:mm a").subtract(1, "years");
-        var currentTime = moment().format("HH:mm a");
-        console.log("Current Time:" + currentTime);
-
-        var timeDiff = moment().diff(moment(firstTrainTime), "minutes");
-        var timeLeft = timeDiff % newFreq;
-        var minutesAway = newFreq - timeLeft;
-        var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
-
-
-        // Store all table content into localStorage
-        localStorage.setItem("name", trainName);
-        localStorage.setItem("destination", destName);
-        localStorage.setItem("frequency", timeFreq);
-        localStorage.setItem("nextArrival", nextArrival);
-        localStorage.setItem("minutesAway", minutesAway);
-
-
-        var newTR = $("<tr>").append(
-            $("<td>").text(localStorage.getItem("name")),
-            $("<td>").text(localStorage.getItem("destination")),
-            $("<td>").text(localStorage.getItem("frequency")),
-            $("<td>").text(localStorage.getItem("nextArrival")),
-            $("<td>").text(localStorage.getItem("minutesAway"))
-
-        );
-        newTR.appendTo("tbody");
-    
-
-        // Clear form
-        $('#train-name').val('');
-        $('#destination-name').val('');
-        $('#time-freq').val('');
-        $('#train-time').val('');
-
-    })
+    $('#train-name').val('');
+    $('#destination-name').val('');
+    $('#time-freq').val('');
+    $('#train-time').val('');
 })
 
-newTR = $("<tr>").append(
-    $("<td>").text(localStorage.getItem("name")),
-    $("<td>").text(localStorage.getItem("destination")),
-    $("<td>").text(localStorage.getItem("frequency")),
-    $("<td>").text(localStorage.getItem("nextArrival")),
-    $("<td>").text(localStorage.getItem("minutesAway"))
-);
-newTR.appendTo("tbody");
+
+//return info to html
+database.ref().on("child_added", function (childSnapshot) {
+    var newName = childSnapshot.val().name;
+    var newDest = childSnapshot.val().destination;
+    var newFreq = childSnapshot.val().frequency;
+    var nextArr = childSnapshot.val().nextArrival;
+    var minAway = childSnapshot.val().minutesAway;
+
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(newName),
+        $("<td>").text(newDest),
+        $("<td>").text(newFreq),
+        $("<td>").text(nextArr),
+        $("<td>").text(minAway)
+    );
+    $(".table > tbody").append(newRow);
+
+    setInterval(function () {
+        minAway--;
+    }, 10000);
+
+    /*var firstTrainTime = moment(newTime, "hh:mm a").subtract(1, "years");
+    var currentTime = moment().format("HH:mm a");
+    console.log("Current Time:" + currentTime);
+
+    var timeDiff = moment().diff(moment(firstTrainTime), "minutes");
+    var timeLeft = timeDiff % newFreq;
+    var minutesAway = newFreq - timeLeft;
+    var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");*/
+
+
+
+    /* Clear form
+    $('#train-name').val('');
+    $('#destination-name').val('');
+    $('#time-freq').val('');
+    $('#train-time').val('');*/
+
+})
+
+
+//if (performance.navigation.type == 1) {
+// alert("the page has been refreshed");
+//}
